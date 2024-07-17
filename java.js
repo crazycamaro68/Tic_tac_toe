11512fc99896ad06819c898c570fe09b9e7471b2
crazycamaro68 <153693685+crazycamaro68@users.noreply.github.com>
2024-07-16 22:33:59 -0400

    Working game. Very rough. Added alot of functions and usable buttons on webpage and also detects winner in console.

--- java.js
+++ java.js
@@ -1,86 +1,133 @@
-function createPlayer(name, icon){
+function createPlayer(name, icon, current){
     const PlayerName = name;
     const PlayerIcon = icon;
+    const currentPlayer = current;
 
-    function makeMove([X,Y]){
-        let postion = [X,Y];
-        let Icon = PlayerIcon;
-        switch (postion.join()){
-            case "0,0":
-                if(game1.tiles[0] === ""){
-                game1.tiles.splice(0,1,Icon);
+    function makeMove(pos){
+        let postion = pos;
+        switch (postion){
+            case "pos00":
+                if(currentGame.tiles[0] === ""){
+                currentGame.tiles.splice(0,1,PlayerIcon);
+                return currentGame.tiles;
                 }else{
                     console.log("Pick Again!");
                 }
             break;
-            case "1,0":
-                if(game1.tiles[1] ===""){
-                game1.tiles.splice(1,1,Icon);
+            case "pos10":
+                if(currentGame.tiles[1] ===""){
+                currentGame.tiles.splice(1,1,PlayerIcon);
+
+                return currentGame.tiles;
                 }else{
                     console.log("Pick Again!");
                 }
             break;
-            case "2,0":
-                if(game1.tiles[2] ===""){
-                game1.tiles.splice(2,1,Icon);
+            case "pos20":
+                if(currentGame.tiles[2] ===""){
+                currentGame.tiles.splice(2,1,PlayerIcon);
+                return currentGame.tiles;
                 }else{
                     console.log("Pick Again!");
                 }
             break;
-            case "0,1":
-                if(game1.tiles[3] ===""){
-                game1.tiles.splice(3,1,Icon);
+            case "pos01":
+                if(currentGame.tiles[3] ===""){
+                currentGame.tiles.splice(3,1,PlayerIcon);
+                return currentGame.tiles;
                 }else{
                     console.log("Pick Again!");
                 }
             break;
-            case "1,1":
-                if(game1.tiles[4] ===""){
-                game1.tiles.splice(4,1,Icon);
+            case "pos11":
+                if(currentGame.tiles[4] ===""){
+                currentGame.tiles.splice(4,1,PlayerIcon);
+                return currentGame.tiles;
                 }else{
                     console.log("Pick Again!");
                 }
             break;
-            case "2,1":
-                if(game1.tiles[5] ===""){
-                game1.tiles.splice(5,1,Icon);
+            case "pos21":
+                if(currentGame.tiles[5] ===""){
+                currentGame.tiles.splice(5,1,PlayerIcon);
+                return currentGame.tiles;
                 }else{
                     console.log("Pick Again!");
                 }
             break;
-            case "0,2":
-                if(game1.tiles[6] ===""){
-                game1.tiles.splice(6,1,Icon);
+            case "pos02":
+                if(currentGame.tiles[6] ===""){
+                currentGame.tiles.splice(6,1,PlayerIcon);
+                return currentGame.tiles;
                 }else{
                     console.log("Pick Again!");
                 }
             break;
-            case "1,2":
-                if(game1.tiles[7] ===""){
-                game1.tiles.splice(7,1,Icon);
+            case "pos12":
+                if(currentGame.tiles[7] ===""){
+                currentGame.tiles.splice(7,1,PlayerIcon);
+                return currentGame.tiles;
                 }else{
                     console.log("Pick Again!");
                 }
             break;
-            case "2,2":
-                if(game1.tiles[8] ===""){
-                game1.tiles.splice(8,1,Icon);
+            case "pos22":
+                if(currentGame.tiles[8] ===""){
+                currentGame.tiles.splice(8,1,PlayerIcon);
+                return currentGame.tiles;
                 }else{
                     console.log("Pick Again!");
                 }
             break;
         }
     }
-    return {PlayerName, PlayerIcon,makeMove}
+    return {PlayerName, PlayerIcon,currentPlayer,makeMove}
 }
 
-function gameboard(){
+function gameBoard(){
     const tiles = ["","","","","","","","",""];
+
     return {tiles};
 }
-
-const Player1 = createPlayer("Paul","X");
-const Player2 = createPlayer("Elyse", "O");
-const game1 = gameboard();
+//NEED to figure out how to store this in a object along with the players and share the info to makeMove Function.
+const currentGame = gameBoard();
 
 
+function startGame(){
+    window.player1 = createPlayer(prompt("Enter player 1 name:"), "X",true);
+    window.player2 = createPlayer(prompt("Enter player 2 name:"), "O",false);
+
+    //Add render function to reset board? add comformaition before completion?
+
+}
+//Checks for winner by checking if all the possible wining spots are the same icons.
+function checkForWinner(player,Icon){
+    if(currentGame.tiles[0] == Icon && currentGame.tiles[1] == Icon && currentGame.tiles[2] == Icon ||
+       currentGame.tiles[3] == Icon && currentGame.tiles[4] == Icon && currentGame.tiles[5] == Icon ||
+       currentGame.tiles[6] == Icon && currentGame.tiles[7] == Icon && currentGame.tiles[8] == Icon ||
+       currentGame.tiles[0] == Icon && currentGame.tiles[3] == Icon && currentGame.tiles[6] == Icon ||
+       currentGame.tiles[1] == Icon && currentGame.tiles[4] == Icon && currentGame.tiles[7] == Icon ||
+       currentGame.tiles[2] == Icon && currentGame.tiles[5] == Icon && currentGame.tiles[8] == Icon ||
+       currentGame.tiles[0] == Icon && currentGame.tiles[4] == Icon && currentGame.tiles[8] == Icon ||
+       currentGame.tiles[2] == Icon && currentGame.tiles[4] == Icon && currentGame.tiles[6] == Icon
+    ){
+        console.log(`Winner is ${player}`);
+    }
+}
+//make use of the onclick on the divs to make a move on the board and then switch witch players turn it is. also checks for winner.
+function makeMove(id){
+    let postion = id.id;
+    if(player1.currentPlayer == true){
+        player1.makeMove(postion);
+        console.log(currentGame.tiles);
+        player1.currentPlayer = false;
+        player2.currentPlayer = true;
+        checkForWinner(player1.PlayerName, player1.PlayerIcon);
+    }else if(player2.currentPlayer == true){
+        player2.makeMove(postion);
+        console.log(currentGame.tiles);
+        player1.currentPlayer = true;
+        player2.currentPlayer = false;
+        checkForWinner(player2.PlayerName ,player2.PlayerIcon);
+    }
+}
